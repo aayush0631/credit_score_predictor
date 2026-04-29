e9d97fa9eff587bde26e821b6f219c876852fd6d
aayush0631 <aayush986532@gmail.com>
2026-04-29 18:08:09 +0545

    docs:add visualization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from pandas.core.groupby import categorical
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -26,12 +27,38 @@ df[['age','credit amount','duration']].hist(bins=7,edgecolor='black')
 plt.suptitle('distribution of numerical features', fontsize=20)
 plt.xlabel('Age')
 plt.show()
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(10,5))
 for i,col in enumerate(['age','credit amount','duration']):
-    plt.subplot(2,2,i+1)
+    plt.subplot(1,3,i+1)
     sns.boxplot(y=df[col],color='skyblue')
     plt.title(col)
 
 plt.tight_layout()
 plt.show()
+# %%
+categorical_cols = ['sex','job','housing','saving accounts','checking account','purpose']
+plt.figure(figsize=(10,10))
 
+for i, col in enumerate(categorical_cols):
+    plt.subplot(3,3,i+1)
+    sns.countplot(data=df, x=col, order=df[col].value_counts().index)
+    plt.title(f'distribution of {col}')
+    plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
+
+# %%
+corr=df[['age','job','credit amount','duration']].corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+plt.show()
+
+#%%
+
+df.groupby('job')['credit amount'].mean()
+#%%
+df.groupby('sex')['credit amount'].mean()
+# we fund male asking higher credit amount
+
+#%%
+pd.pivot_table(df,values='credit amount',index='housing',columns='purpose')
